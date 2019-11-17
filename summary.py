024d77ed9d775292caebf6ee79d903582e8a0952
Pablo Jadzinsky <pjadzinsky@gmail.com>
2019-11-16 17:04:50 -0800

    Changing dates from string to datetime.date objects in summary Adding to scatter plots

--- summary.py
+++ summary.py
@@ -1,5 +1,8 @@
-import pandas as pd
+import holoviews as hv
 import numpy as np
+import pandas as pd
+from datetime import datetime, date
+
 
 from config import ZILLOWED_FOLDER
 from auction import load_last_df
@@ -17,11 +20,39 @@ def main():
     print(df.columns)
     df.loc[:, 'sold_to_estimated_ratio'] = df.zillow_last_sold_price / df.after_repair_value
     df.sort_values(by='sold_to_estimated_ratio', inplace=True)
+    df.loc[:, 'zillow_last_date_sold'] = df.zillow_last_date_sold.apply(lambda x:
+                                                                        datetime.strptime(x, '%m/%d/%Y').date())
 
     selected_columns = [
         'sold_to_estimated_ratio', 'after_repair_value', 'online_event', 'status_label'
     ]
     print(df[selected_columns])
+    scatter_plot_1(df)
+    scatter_plot_2(df)
+
+
+def scatter_plot_1(df):
+    """ Produce a scatter plot of
+    'zillow_last_date_sold' vs 'sold_to_estimated_ratio'
+    """
+    kdims = ['zillow_last_date_sold', ]
+    vdims = ['sold_to_estimated_ratio', 'zillow_last_sold_price']
+    data = hv.Dataset(df, kdims, vdims)
+    renderer = hv.renderer('bokeh')
+    crv = data.to(hv.Scatter, 'zillow_last_date_sold', 'sold_to_estimated_ratio')
+    renderer.save(crv, '/tmp/scatter_1')
+
+
+def scatter_plot_2(df):
+    """ Produce a scatter plot of 
+    'zillow_last_sold_price' vs 'sold_to_estimated_ratio'
+    """
+    kdims = ['zillow_last_sold_price']
+    vdims = ['sold_to_estimated_ratio']
+    data = hv.Dataset(df, kdims, vdims)
+    renderer = hv.renderer('bokeh')
+    crv = data.to(hv.Scatter, 'zillow_last_sold_price', 'sold_to_estimated_ratio')
+    renderer.save(crv, '/tmp/scatter_2')
 
 
 def merge_estimates(df):
